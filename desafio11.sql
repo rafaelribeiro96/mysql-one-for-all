765f3ca6b5a65c834a6abc5c4a1fa1179e56f919
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-10-26 14:01:56 -0300

    requisito 11 realizado

---
+++ desafio11.sql
@@ -0,0 +1,19 @@
+SELECT 
+  music_name AS nome_musica,
+	CASE
+		  WHEN music_name LIKE '%Bard%' THEN REPLACE(music_name, 'Bard', 'QA')
+      WHEN music_name LIKE '%Amar%' THEN REPLACE(music_name, 'Amar', 'Code Review')
+      WHEN music_name LIKE '%Pais%' THEN REPLACE(music_name, 'Pais', 'Pull Requests')
+      WHEN music_name LIKE '%SOUL%' THEN REPLACE(music_name, 'SOUL', 'CODE')
+      WHEN music_name LIKE '%SUPERSTAR%' THEN REPLACE(music_name, 'SUPERSTAR', 'SUPERDEV')
+      ELSE music_name
+    END AS novo_nome
+  FROM 
+    music
+  WHERE
+	  music_name LIKE '%Bard%'
+      OR music_name LIKE '%Amar%'
+      OR music_name LIKE '%Pais%'
+	    OR music_name LIKE '%SOUL%'
+      OR music_name LIKE '%SUPERSTAR%'
+	ORDER BY music_name DESC;
